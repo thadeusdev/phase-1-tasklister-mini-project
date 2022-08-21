2af6fc377efa98e052c891b0f5f92760ecbb223c
thadeus <thadeusnyariki94@gmail.com>
2022-08-21 14:21:16 +0300

    Created a toDo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// make our todo list
+const newTaskDescription = document.getElementById("new-task-description");
+const toDoList = document.getElementById("tasks");
+
+const createNewTask = (event) => { 
+  event.preventDefault(); 
+  const newTask = document.createElement('li');
+
+  newTask.innerText = newTaskDescription.value;
+  createButton(newTask);
+  toDoList.appendChild(newTask);
+
+  event.target.reset();
+};
+
+const createButton = (task) => {
+   const btn = document.createElement('button');
+    btn.innerText = "X";
+
+    task.appendChild(btn);
+    btn.addEventListener('click',  (e) => deleteTask(e));
+}
+
+const deleteTask = (e) => {
+  const selectedTask = e.target.parentElement;
+  toDoList.removeChild(selectedTask);
+}
+
+document.querySelector('#create-task-form').addEventListener("submit"
+, (e) => createNewTask(e))
